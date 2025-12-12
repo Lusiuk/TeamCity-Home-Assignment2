b981812b9d7218d9c8e689af7bed7ba4023f68c4
Lusiuk <brcloud1@yandex.ru>
2025-12-12 20:50:07 +0300

    Created simple project

---
+++ rust_project/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello from Rust!");
+    
+    let a: u32 = 3;
+    let b: u32 = 4;
+    let c: u32 = 5;
+    
+    if a.pow(2) + b.pow(2) == c.pow(2) {
+        println!("Пифагорова тройка найдена: {}^2 + {}^2 = {}^2", a, b, c);
+    } else {
+        println!("Пифагорова тройка не найдена");
+    }
+}
