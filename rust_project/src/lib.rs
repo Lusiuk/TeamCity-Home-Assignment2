10173e5351a10c1c4869f6a82a8f7123fe4df464
Lusiuk <brcloud1@yandex.ru>
2025-12-12 20:50:55 +0300

    Added simple tests

---
+++ rust_project/src/lib.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_multiplication() {
+        assert_eq!(3 * 3, 9);
+    }
+
+    #[test]
+    fn test_pifagor() {
+        let a = 3u32;
+        let b = 4u32;
+        let c = 5u32;
+        assert_eq!(a.pow(2) + b.pow(2), c.pow(2));
+    }
+}
